bd7e8a01cb39b990325e3b285d8ca9ec87f6fe5b
LindseySongstad <lindsey.songstad@outlook.com>
2021-09-14 09:32:35 -0700

    valid palindrome

---
+++ ValidPalindrome/ValidPalindrome/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace ValidPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "`l;`` 1o1 ??;l`";
+            Solution sol = new Solution();
+            Console.WriteLine(sol.IsPalindrome(s));
+        }
+        public class Solution
+        {
+            public bool IsPalindrome(string s)
+            {
+                s = s.ToLower();
+                StringBuilder sb = new StringBuilder();
+                foreach(char c in s)
+                {
+                    if (char.IsLetterOrDigit(c))
+                    {
+                        sb.Append(c);
+                    }
+                }
+                for (int i = 0; i < sb.Length/2; i++)
+                {
+                    int j = sb.Length -1 - i;
+                    if (sb[i] != sb[j]) return false; 
+                }
+                return true;
+            }
+        }
+    }
+}
